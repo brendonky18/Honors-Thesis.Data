44abede7ef1b124415674eeb7b3f5a673d349688
Brendon Ky <brendonky18@users.noreply.github.com>
2021-12-12 01:10:59 -0500

    updated tex filter for refactoring changes

--- tex-filter.py
+++ tex-filter.py
@@ -46,35 +46,35 @@ def clean():
         # captures punctuation marks, but ignores those in comments
         commentRegex = f"(?(?=%)(?:.*)\n|{punctuationRegex})"
 
-        if "\\begin{document}" in line:
-            readingFromDocBody = True
-        elif "\\end{document}" in line:
-            readingFromDocBody = False        
+        # if "\\begin{document}" in line:
+        #     readingFromDocBody = True
+        # elif "\\end{document}" in line:
+        #     readingFromDocBody = False        
 
-        if readingFromDocBody:
-            splitList = regex.split(commentRegex, line)
-            # print(splitList)
-            resultListItr = iter(splitList)
+        # if readingFromDocBody:
+        splitList = regex.split(commentRegex, line)
+        # print(splitList)
+        resultListItr = iter(splitList)
 
-            curItem = next(resultListItr, None)
-            nextItem = next(resultListItr, None)
-            while nextItem is not None:
-                # next item is punctuation, so combine them
-                # print(f"cur item:{curItem}")
-                # print(f"next item:{nextItem}")
-                # print(f"regex match: {regex.match(punctuationRegex, nextItem)}")
-                if curItem.endswith('\n'):
-                    print(f"{curItem}")
-                elif regex.match(punctuationRegex, nextItem) is not None:
-                    print(f"{curItem + nextItem}", end=NL_REPLACE)
-                    nextItem = next(resultListItr, None)
-                else:
-                    print(f"{curItem}")
-                
-                curItem, nextItem = nextItem, next(resultListItr, None)
-            print(f"{curItem}", end="")
-        else:
-            print(f"{line}", end="")
+        curItem = next(resultListItr, None)
+        nextItem = next(resultListItr, None)
+        while nextItem is not None:
+            # next item is punctuation, so combine them
+            # print(f"cur item:{curItem}")
+            # print(f"next item:{nextItem}")
+            # print(f"regex match: {regex.match(punctuationRegex, nextItem)}")
+            if curItem.endswith('\n'):
+                print(f"{curItem}")
+            elif regex.match(punctuationRegex, nextItem) is not None:
+                print(f"{curItem + nextItem}", end=NL_REPLACE)
+                nextItem = next(resultListItr, None)
+            else:
+                print(f"{curItem}")
+            
+            curItem, nextItem = nextItem, next(resultListItr, None)
+        print(f"{curItem}", end="")
+        # else:
+        #     print(f"{line}", end="")
 
 
 if __name__ == '__main__':
